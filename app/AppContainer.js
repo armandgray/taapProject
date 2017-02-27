4bde4206a8ccf98d8406fea63b8233a89fbab5be
Armand Gray <armandgray@gmail.com>
2017-02-26 20:44:21 -0600

    added Navigator

--- app/AppContainer.js
+++ app/AppContainer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Drawer } from 'native-base'
-import { View } from 'react-native'
+import { View, Navigator } from 'react-native'
 
 export default class AppContainer extends Component {
 	constructor(props) {
@@ -15,13 +15,22 @@ export default class AppContainer extends Component {
 	toggleDrawer() {
 		this.state.toggled ? this._drawer.close() : this._drawer.open()
 	}
-
 	openDrawer() {
 		this.setState({toggled: true})
 	}
-
 	closeDrawer() {
 		this.setState({toggled: false})
+	}
+
+	renderScene(route, navigator) {
+		switch(route) {
+			default: {
+				return null
+			}
+		}
+	}
+	configureScene(route, routeStack) {
+		return Navigator.SceneConfigs.PushFromRight
 	}
 
 	render() {
@@ -29,10 +38,16 @@ export default class AppContainer extends Component {
 			<Drawer
 				ref = {(ref) => this._drawer = ref}
 				type = 'displace'
-				content = { <View style = {{ backgroundColor: '#000', heigth: 1000 }} /> }
+				content = { <View style = {{ backgroundColor: '#000', height: 1000 }} /> }
 				onClose = {this.closeDrawer.bind(this)}
 				onOpen = {this.openDrawer.bind(this)}
-				openDrawerOffset = {0.2}
+				openDrawerOffset = {0.2}>
+
+				<Navigator 
+					ref = {(ref) => this._navigator = ref}
+					configureScene = {this.configureScene.bind(this)}
+					renderScene = {this.renderScene.bind(this)}>
+				</Navigator>
 			</Drawer>
 		)
 	}
