4ddaaaa9a48a347638d398ab27578e1942829492
Armand Gray <armandgray@gmail.com>
2017-02-26 20:24:25 -0600

    exported AppContainer with TextView; replaced all contents of index.*

--- app/AppContainer.js
+++ app/AppContainer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Text, View } from 'react-native'
 
 export default class AppContainer extends Component {
 	constructor(props) {
@@ -10,6 +11,10 @@ export default class AppContainer extends Component {
 	}
 
 	render() {
-		return null
+		return (
+			<View>
+				<Text>Hello World</Text>
+			</View>
+		)
 	}
 }
