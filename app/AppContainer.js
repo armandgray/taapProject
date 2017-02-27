f268470603c9bfff2cdf7c531cd29ec804e4bbb4
Armand Gray <armandgray@gmail.com>
2017-02-26 20:35:03 -0600

    added Drawer to AppContainer

--- app/AppContainer.js
+++ app/AppContainer.js
@@ -1,20 +1,39 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Drawer } from 'native-base'
+import { View } from 'react-native'
 
 export default class AppContainer extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
+			toggled: false,
 			store: {},
 			theme: null
 		}
 	}
 
+	toggleDrawer() {
+		this.state.toggled ? this._drawer.close() : this._drawer.open()
+	}
+
+	openDrawer() {
+		this.setState({toggled: true})
+	}
+
+	closeDrawer() {
+		this.setState({toggled: false})
+	}
+
 	render() {
 		return (
-			<View>
-				<Text>Hello World</Text>
-			</View>
+			<Drawer
+				ref = {(ref) => this._drawer = ref}
+				type = 'displace'
+				content = { <View style = {{ backgroundColor: '#000', heigth: 1000 }} /> }
+				onClose = {this.closeDrawer.bind(this)}
+				onOpen = {this.openDrawer.bind(this)}
+				openDrawerOffset = {0.2}
+			</Drawer>
 		)
 	}
 }
