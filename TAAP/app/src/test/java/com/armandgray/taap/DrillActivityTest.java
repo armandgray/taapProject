fc12e57e25184b6c85e4d7fceb5652c92345bb5b
Armand Gray <armandgray@gmail.com>
2017-04-11 15:14:47 -0700

    Added setup and teardown

--- TAAP/app/src/test/java/com/armandgray/taap/DrillActivityTest.java
+++ TAAP/app/src/test/java/com/armandgray/taap/DrillActivityTest.java
@@ -1,12 +1,13 @@
 package com.armandgray.taap;
 
-import android.app.Activity;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.widget.Spinner;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
@@ -21,23 +22,30 @@ import static org.robolectric.Shadows.shadowOf;
 @Config(constants = BuildConfig.class)
 public class DrillActivityTest {
 
+    private static DrillActivity activity;
+
+    @Before
+    public void setUp() {
+        System.out.println("Running Set Up!");
+        if (activity == null) {
+            activity = Robolectric.buildActivity(DrillActivity.class).create().visible().get();
+        }
+    }
+
     @Test
     public void testContainsView_ToolBar() throws Exception {
-        Activity activity = Robolectric.buildActivity(DrillActivity.class).create().visible().get();
         Toolbar toolbar = (Toolbar) activity.findViewById(R.id.toolbar);
         assertNotNull(toolbar);
     }
 
     @Test
     public void testContainsView_ToolBarSpinner() throws Exception {
-        Activity activity = Robolectric.buildActivity(DrillActivity.class).create().visible().get();
         Spinner spinner = (Spinner) activity.findViewById(R.id.spDrillsSort);
         assertNotNull(spinner);
     }
 
     @Test
     public void testContainsView_ToolBarSearch() throws Exception {
-        Activity activity = Robolectric.buildActivity(DrillActivity.class).create().visible().get();
         Toolbar toolbar = (Toolbar) activity.findViewById(R.id.toolbar);
         shadowOf(activity).onCreateOptionsMenu(toolbar.getMenu());
         Menu optionsMenu = shadowOf(activity).getOptionsMenu();
@@ -47,14 +55,12 @@ public class DrillActivityTest {
 
     @Test
     public void testExistsRes_SpinnerEntries() throws Exception {
-        Activity activity = Robolectric.buildActivity(DrillActivity.class).create().visible().get();
         String[] drillsArray = activity.getResources().getStringArray(R.array.drills_array);
         assertTrue(drillsArray.length > 0);
     }
 
     @Test
     public void testSpinnerHasEntries_ToolBarSpinner() throws Exception {
-        Activity activity = Robolectric.buildActivity(DrillActivity.class).create().visible().get();
         Spinner spinner = (Spinner) activity.findViewById(R.id.spDrillsSort);
         assertNotNull(spinner);
         assertTrue(spinner.getCount() > 0);
@@ -62,16 +68,20 @@ public class DrillActivityTest {
 
     @Test
     public void testContainsView_Fab() throws Exception {
-        Activity activity = Robolectric.buildActivity(DrillActivity.class).create().visible().get();
         FloatingActionButton fab = (FloatingActionButton) activity.findViewById(R.id.fab);
         assertNotNull(fab);
     }
 
     @Test
     public void testContainsView_RvDrills() throws Exception {
-        Activity activity = Robolectric.buildActivity(DrillActivity.class).create().visible().get();
         RecyclerView rvDrills = (RecyclerView) activity.findViewById(R.id.rvDrills);
         assertNotNull(rvDrills);
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("Running TearDown!");
+        activity = null;
     }
 
 }
