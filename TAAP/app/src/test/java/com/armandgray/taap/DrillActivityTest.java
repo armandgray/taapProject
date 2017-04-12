85e11faed5eb99d10113511a8d654080f398e5dd
Armand Gray <armandgray@gmail.com>
2017-04-12 12:23:25 -0700

    added code to canCreateSpinnerAdapter

--- TAAP/app/src/test/java/com/armandgray/taap/DrillActivityTest.java
+++ TAAP/app/src/test/java/com/armandgray/taap/DrillActivityTest.java
@@ -1,13 +1,26 @@
 package com.armandgray.taap;
 
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
 import org.junit.Test;
+import org.mockito.Mock;
+
+import java.lang.reflect.Method;
 
 import static junit.framework.Assert.assertNotNull;
 
 public class DrillActivityTest {
 
+    @Mock
+    private Context context;
+
     @Test
     public void canCreateSpinnerAdapter() throws Exception {
-        assertNotNull("");
+        Method createSpinnerAdapter = DrillActivity.class.getDeclaredMethod("createSpinnerAdapter", (Class<?>) null);
+        createSpinnerAdapter.setAccessible(true);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(context, 0);
+        createSpinnerAdapter.invoke(adapter, (Object) null);
+        assertNotNull(adapter);
     }
 }
