8581cfe330e66274561499833b09965d469ac2d1
Armand Gray <armandgray@gmail.com>
2017-04-12 12:16:03 -0700

    Added test canCreateSpinnerAdapter in DrillActivityTest

---
+++ TAAP/app/src/test/java/com/armandgray/taap/DrillActivityTest.java
@@ -0,0 +1,13 @@
+package com.armandgray.taap;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertNotNull;
+
+public class DrillActivityTest {
+
+    @Test
+    public void canCreateSpinnerAdapter() throws Exception {
+        assertNotNull("");
+    }
+}
