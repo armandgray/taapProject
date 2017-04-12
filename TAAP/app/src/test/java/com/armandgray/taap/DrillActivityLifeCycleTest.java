f7112b8817660391110725e9004af2708af4fa29
Armand Gray <armandgray@gmail.com>
2017-04-12 13:27:58 -0700

    added DrillActivityLifeCycleTest.java canGetContentView_TestOnCreate

--- TAAP/app/src/test/java/com/armandgray/taap/DrillActivityLifeCycleTest.java
+++ TAAP/app/src/test/java/com/armandgray/taap/DrillActivityLifeCycleTest.java
@@ -2,8 +2,16 @@ package com.armandgray.taap;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
+import static junit.framework.Assert.assertNotNull;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class)
 public class DrillActivityLifeCycleTest {
 
     private static DrillActivity activity;
@@ -16,9 +24,15 @@ public class DrillActivityLifeCycleTest {
         }
     }
 
+    @Test
+    public void canGetContentView_TestOnCreate() throws Exception {
+        assertNotNull(null);
+    }
+
     @After
     public void tearDown() {
         System.out.println("Running TearDown!");
         activity = null;
     }
+
 }
