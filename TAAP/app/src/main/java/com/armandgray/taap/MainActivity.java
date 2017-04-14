ba038f5005cd5400bc8e204cbaf933f1b7f4047b
Armand Gray <armandgray@gmail.com>
2017-04-14 11:58:39 -0700

    refactored all code from mainActivity to Controller

--- TAAP/app/src/main/java/com/armandgray/taap/MainActivity.java
+++ TAAP/app/src/main/java/com/armandgray/taap/MainActivity.java
@@ -2,43 +2,20 @@ package com.armandgray.taap;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
     public MainActivityController controller;
-
-    String[] drillsArray = {"All", "Shooting", "Ball Handling", "Passing", "Fundamentals"};
-    private SearchView searchView;
-    private FloatingActionButton fab;
-    private Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         controller = new MainActivityController(this);
-
-        fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // TODO Add onClick action to fab
-            }
-        });
-
-        spinner = (Spinner) findViewById(R.id.spDrillsSort);
-        spinner.setAdapter(createSpinnerAdapter());
-
-        searchView = (SearchView) findViewById(R.id.searchView);
-        searchView.setVisibility(View.GONE);
     }
 
     @Override
@@ -51,9 +28,9 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_search:
-                searchView.setVisibility(View.VISIBLE);
-                fab.setVisibility(View.GONE);
-                spinner.setVisibility(View.GONE);
+                controller.searchView.setVisibility(View.VISIBLE);
+                controller.fab.setVisibility(View.GONE);
+                controller.spinner.setVisibility(View.GONE);
                 return true;
             case R.id.action_log:
                 startActivity(new Intent());
@@ -61,10 +38,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    private ArrayAdapter<String> createSpinnerAdapter() {
-        return new ArrayAdapter<>(this,
-                R.layout.spinner_drills_text_layout, R.id.tvSpinnerDrill, drillsArray);
     }
 }
--- TAAP/app/src/main/java/com/armandgray/taap/MainActivityController.java
+++ TAAP/app/src/main/java/com/armandgray/taap/MainActivityController.java
@@ -1,9 +1,20 @@
 package com.armandgray.taap;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 class MainActivityController {
     MainActivity activity;
+
+    private String[] drillsArray = {"All", "Shooting", "Ball Handling", "Passing", "Fundamentals"};
+
+    SearchView searchView;
+    FloatingActionButton fab;
+    Spinner spinner;
 
     MainActivityController(MainActivity activity) {
         this.activity = activity;
@@ -14,5 +25,24 @@ class MainActivityController {
         activity.setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) activity.findViewById(R.id.toolbar);
         activity.setSupportActionBar(toolbar);
+
+        fab = (FloatingActionButton) activity.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO Add onClick action to fab
+            }
+        });
+
+        spinner = (Spinner) activity.findViewById(R.id.spDrillsSort);
+        spinner.setAdapter(createSpinnerAdapter());
+
+        searchView = (SearchView) activity.findViewById(R.id.searchView);
+        searchView.setVisibility(View.GONE);
+    }
+
+    private ArrayAdapter<String> createSpinnerAdapter() {
+        return new ArrayAdapter<>(activity,
+                R.layout.spinner_drills_text_layout, R.id.tvSpinnerDrill, drillsArray);
     }
 }
