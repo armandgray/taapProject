ff95a992b656a674cb3c1254bd96f8d2cacd667a
Armand Gray <armandgray@gmail.com>
2017-04-14 15:54:36 -0700

    moved all res tests into new class and refactored ViewsTest code

---
+++ TAAP/app/src/test/java/com/armandgray/taap/MainActivityResTest.java
@@ -0,0 +1,88 @@
+package com.armandgray.taap;
+
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+import static org.robolectric.Shadows.shadowOf;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class)
+public class MainActivityResTest {
+
+    private MainActivity activity;
+    private MainActivityViews views;
+    private Toolbar toolbar;
+    private Menu optionsMenu;
+
+    @Before
+    public void setUp() {
+        System.out.println("Running Set Up!");
+        activity = Robolectric.buildActivity(MainActivity.class).create().visible().get();
+        toolbar = (Toolbar) activity.findViewById(R.id.toolbar);
+        shadowOf(activity).onCreateOptionsMenu(toolbar.getMenu());
+        optionsMenu = shadowOf(activity).getOptionsMenu();
+    }
+
+    @Test
+    public void hasText_ToolBarTitle() throws Exception {
+        assertEquals("TAAP", toolbar.getTitle());
+    }
+
+    @Test
+    public void hasView_ToolBarSpinner() throws Exception {
+        Spinner spinner = (Spinner) activity.findViewById(R.id.spDrillsSort);
+        assertNotNull(spinner);
+    }
+
+    @Test
+    public void existsRes_SpinnerLayout() throws Exception {
+        TextView tvSpinnerLayout = (TextView) View.inflate(activity, R.layout.spinner_drills_text_layout, null);
+        assertNotNull(tvSpinnerLayout);
+    }
+
+    @Test
+    public void hasOptionsMenuItem_Search() throws Exception {
+        assertNotNull(optionsMenu.findItem(R.id.action_search));
+    }
+
+    @Test
+    public void hasOptionsMenuItem_Log() throws Exception {
+        assertNotNull(optionsMenu.findItem(R.id.action_log));
+    }
+
+    @Test
+    public void hasView_SearchView() throws Exception {
+        SearchView searchView = (SearchView) activity.findViewById(R.id.searchView);
+        assertNotNull(searchView);
+    }
+
+    @Test
+    public void hasView_RvDrills() throws Exception {
+        RecyclerView rvDrills = (RecyclerView) activity.findViewById(R.id.rvDrills);
+        assertNotNull(rvDrills);
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("Running TearDown!");
+        activity = null;
+        toolbar = null;
+        optionsMenu = null;
+    }
+
+}
--- TAAP/app/src/test/java/com/armandgray/taap/MainActivityViewsTest.java
+++ TAAP/app/src/test/java/com/armandgray/taap/MainActivityViewsTest.java
@@ -1,13 +1,5 @@
 package com.armandgray.taap;
 
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.SearchView;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.View;
-import android.widget.Spinner;
-import android.widget.TextView;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,8 +9,6 @@ import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
 import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertNotNull;
-import static org.robolectric.Shadows.shadowOf;
 
 @RunWith(RobolectricTestRunner.class)
 @Config(constants = BuildConfig.class)
@@ -26,17 +16,12 @@ public class MainActivityViewsTest {
 
     private MainActivity activity;
     private MainActivityViews views;
-    private Toolbar toolbar;
-    private Menu optionsMenu;
 
     @Before
     public void setUp() {
         System.out.println("Running Set Up!");
         activity = Robolectric.buildActivity(MainActivity.class).create().visible().get();
         views = activity.controller.views;
-        toolbar = (Toolbar) activity.findViewById(R.id.toolbar);
-        shadowOf(activity).onCreateOptionsMenu(toolbar.getMenu());
-        optionsMenu = shadowOf(activity).getOptionsMenu();
     }
 
     @Test
@@ -44,51 +29,11 @@ public class MainActivityViewsTest {
         assertEquals("MainActivity", views.activity.getLocalClassName());
     }
 
-    @Test
-    public void hasText_ToolBarTitle() throws Exception {
-        assertEquals("TAAP", toolbar.getTitle());
-    }
-
-    @Test
-    public void hasView_ToolBarSpinner() throws Exception {
-        Spinner spinner = (Spinner) activity.findViewById(R.id.spDrillsSort);
-        assertNotNull(spinner);
-    }
-
-    @Test
-    public void existsRes_SpinnerLayout() throws Exception {
-        TextView tvSpinnerLayout = (TextView) View.inflate(activity, R.layout.spinner_drills_text_layout, null);
-        assertNotNull(tvSpinnerLayout);
-    }
-
-    @Test
-    public void hasOptionsMenuItem_Search() throws Exception {
-        assertNotNull(optionsMenu.findItem(R.id.action_search));
-    }
-
-    @Test
-    public void hasOptionsMenuItem_Log() throws Exception {
-        assertNotNull(optionsMenu.findItem(R.id.action_log));
-    }
-
-    @Test
-    public void hasView_SearchView() throws Exception {
-        SearchView searchView = (SearchView) activity.findViewById(R.id.searchView);
-        assertNotNull(searchView);
-    }
-
-    @Test
-    public void hasView_RvDrills() throws Exception {
-        RecyclerView rvDrills = (RecyclerView) activity.findViewById(R.id.rvDrills);
-        assertNotNull(rvDrills);
-    }
-
     @After
     public void tearDown() {
         System.out.println("Running TearDown!");
         activity = null;
-        toolbar = null;
-        optionsMenu = null;
+        views = null;
     }
 
 }
