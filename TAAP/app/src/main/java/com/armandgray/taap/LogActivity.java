df47410575127123200440e2ddc6cc915a6a17b8
Armand Gray <armandgray@gmail.com>
2017-04-18 09:35:26 -0700

    removed content from LogActivity onCreate/

--- TAAP/app/src/main/java/com/armandgray/taap/LogActivity.java
+++ TAAP/app/src/main/java/com/armandgray/taap/LogActivity.java
@@ -2,20 +2,12 @@ package com.armandgray.taap;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
 public class LogActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_log);
-        
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        }
     }
 
 }
