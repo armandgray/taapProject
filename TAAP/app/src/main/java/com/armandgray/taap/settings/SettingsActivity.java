b38a03dfccd0920f004329143463655e86cc3256
Armand Gray <armandgray@gmail.com>
2017-04-18 08:40:53 -0700

    removed fab from Log/SettingsActivity

--- TAAP/app/src/main/java/com/armandgray/taap/settings/SettingsActivity.java
+++ TAAP/app/src/main/java/com/armandgray/taap/settings/SettingsActivity.java
@@ -1,11 +1,8 @@
 package com.armandgray.taap.settings;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import com.armandgray.taap.R;
 
@@ -19,19 +16,11 @@ public class SettingsActivity extends AppCompatActivity {
 
         controller = new SettingsActivityController(this);
 
-        setContentView(R.layout.activity_settings);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
     }
 
 }
