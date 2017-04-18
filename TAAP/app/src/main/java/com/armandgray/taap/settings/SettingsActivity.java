8ffdaf5e06485894a48d39033f5ad14c062966f6
Armand Gray <armandgray@gmail.com>
2017-04-18 08:36:16 -0700

    replaced LogActivity & SettingsActivity with BasicActivity aka toolbar

--- TAAP/app/src/main/java/com/armandgray/taap/settings/SettingsActivity.java
+++ TAAP/app/src/main/java/com/armandgray/taap/settings/SettingsActivity.java
@@ -1,7 +1,11 @@
 package com.armandgray.taap.settings;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.armandgray.taap.R;
 
@@ -14,5 +18,20 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         controller = new SettingsActivityController(this);
+
+        setContentView(R.layout.activity_settings);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
 }
