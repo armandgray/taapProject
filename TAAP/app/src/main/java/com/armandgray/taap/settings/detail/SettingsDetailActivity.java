3e68fbec33ef569975afc674c7d1a8b3e9c679c7
Armand Gray <armandgray@gmail.com>
2017-04-18 17:57:13 -0700

    refactored toolbar code

--- TAAP/app/src/main/java/com/armandgray/taap/settings/detail/SettingsDetailActivity.java
+++ TAAP/app/src/main/java/com/armandgray/taap/settings/detail/SettingsDetailActivity.java
@@ -15,14 +15,22 @@ public class SettingsDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_detail);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        ((TextView) toolbar.findViewById(R.id.tvTitle))
-                .setText(getIntent().getStringExtra(SELECTED_ITEM));
-        setSupportActionBar(toolbar);
+        setupToolbar();
+    }
+
+    private void setupToolbar() {
+        setSupportActionBarAsToolbar();
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayShowTitleEnabled(false);
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
+    }
+
+    private void setSupportActionBarAsToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        ((TextView) toolbar.findViewById(R.id.tvTitle))
+                .setText(getIntent().getStringExtra(SELECTED_ITEM));
+        setSupportActionBar(toolbar);
     }
 
 }
