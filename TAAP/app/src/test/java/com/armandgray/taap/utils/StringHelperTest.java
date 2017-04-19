e9824c2011b18e4e49d8eba0c76aef7a69ae79ca
Armand Gray <armandgray@gmail.com>
2017-04-18 18:17:47 -0700

    Added test canGetFormattedHeaderTextString

--- TAAP/app/src/test/java/com/armandgray/taap/utils/StringHelperTest.java
+++ TAAP/app/src/test/java/com/armandgray/taap/utils/StringHelperTest.java
@@ -1,5 +1,16 @@
 package com.armandgray.taap.utils;
 
+import org.junit.Test;
+
+import static junit.framework.Assert.assertNotNull;
+
 public class StringHelperTest {
 
+    @Test
+    public void canGetFormattedHeaderTextString() throws Exception {
+        String[] headers = {"Evox Images", "LibphoneNumber", "Lottie for iOS"};
+        String[] text = new String[3];
+        text[0] = "Vehicle photos licensed from Evox Images (C) 2014.\n\nhttp://www.evoximages.com";
+        assertNotNull(getFormattedHeaderTextString(headers, text));
+    }
 }
