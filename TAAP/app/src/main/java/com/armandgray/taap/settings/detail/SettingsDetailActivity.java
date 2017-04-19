f80767a6ca8c90a10ae40541c121bb514adc0340
Armand Gray <armandgray@gmail.com>
2017-04-19 15:17:29 -0700

    added setTvContentsText to pass setText test

--- TAAP/app/src/main/java/com/armandgray/taap/settings/detail/SettingsDetailActivity.java
+++ TAAP/app/src/main/java/com/armandgray/taap/settings/detail/SettingsDetailActivity.java
@@ -7,7 +7,10 @@ import android.widget.TextView;
 
 import com.armandgray.taap.R;
 
+import static com.armandgray.taap.settings.SettingsActivityController.COPYRIGHT;
 import static com.armandgray.taap.settings.SettingsActivityController.SELECTED_ITEM;
+import static com.armandgray.taap.settings.SettingsActivityController.SOFTWARE_LICENSES;
+import static com.armandgray.taap.settings.SettingsActivityController.TERMS_AND_CONDITIONS;
 
 public class SettingsDetailActivity extends AppCompatActivity {
 
@@ -16,6 +19,7 @@ public class SettingsDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_detail);
         setupToolbar();
+        setTvContentsText();
     }
 
     private void setupToolbar() {
@@ -31,6 +35,21 @@ public class SettingsDetailActivity extends AppCompatActivity {
         ((TextView) toolbar.findViewById(R.id.tvTitle))
                 .setText(getIntent().getStringExtra(SELECTED_ITEM));
         setSupportActionBar(toolbar);
+    }
+
+    private void setTvContentsText() {
+        TextView tvContents = (TextView) findViewById(R.id.tvContents);
+        switch (getIntent().getStringExtra(SELECTED_ITEM)) {
+            case COPYRIGHT:
+                tvContents.setText(COPYRIGHT);
+                break;
+            case TERMS_AND_CONDITIONS:
+                tvContents.setText(TERMS_AND_CONDITIONS);
+                break;
+            case SOFTWARE_LICENSES:
+                tvContents.setText(SOFTWARE_LICENSES);
+                break;
+        }
     }
 
 }
