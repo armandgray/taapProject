4e187ed9b16ae6fd8a7f8b6583348e9e2397ea4a
Armand Gray <armandgray@gmail.com>
2017-04-20 12:42:40 -0700

    Added DrillAdapter(arrayList) constructor

--- TAAP/app/src/main/java/com/armandgray/taap/utils/DrillsRvAdapter.java
+++ TAAP/app/src/main/java/com/armandgray/taap/utils/DrillsRvAdapter.java
@@ -1,6 +1,14 @@
 package com.armandgray.taap.utils;
 
+import com.armandgray.taap.models.Drill;
+
+import java.util.ArrayList;
+
 public class DrillsRvAdapter {
+
+    public DrillsRvAdapter(ArrayList<Drill> drillList) {
+
+    }
 
     public int getItemCount() {
         return 0;
--- TAAP/app/src/test/java/com/armandgray/taap/utils/DrillsRvAdapterTest.java
+++ TAAP/app/src/test/java/com/armandgray/taap/utils/DrillsRvAdapterTest.java
@@ -3,6 +3,8 @@ package com.armandgray.taap.utils;
 import android.view.View;
 
 import com.armandgray.taap.BuildConfig;
+import com.armandgray.taap.R;
+import com.armandgray.taap.models.Drill;
 
 import org.junit.After;
 import org.junit.Before;
@@ -10,6 +12,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
+
+import java.util.ArrayList;
 
 import static junit.framework.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
@@ -24,12 +28,18 @@ public class DrillsRvAdapterTest {
     @Before
     public void setUp() {
         System.out.println("Running Set Up!");
-        adapter = new DrillsRvAdapter();
+        adapter = new DrillsRvAdapter(null);
         mockView = mock(View.class);
     }
 
     @Test
     public void canGetItemCount() throws Exception {
+        Drill drill = new Drill("2-Ball Pound Dribble", R.drawable.ic_fitness_center_white_24dp);
+        ArrayList<Drill> drillList = new ArrayList<>();
+        drillList.add(drill);
+        drillList.add(drill);
+        drillList.add(drill);
+        adapter = new DrillsRvAdapter(drillList);
         assertEquals(3, adapter.getItemCount());
     }
 
