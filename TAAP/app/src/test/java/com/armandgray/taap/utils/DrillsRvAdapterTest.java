47a9388134da5259602c90f75aefed5f1cf43d85
Armand Gray <armandgray@gmail.com>
2017-04-20 11:07:01 -0700

    added DrillsRvAdapter field

--- TAAP/app/src/test/java/com/armandgray/taap/utils/DrillsRvAdapterTest.java
+++ TAAP/app/src/test/java/com/armandgray/taap/utils/DrillsRvAdapterTest.java
@@ -6,19 +6,19 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
-
-import static junit.framework.Assert.assertNotNull;
 
 @RunWith(RobolectricTestRunner.class)
 @Config(constants = BuildConfig.class)
 public class DrillsRvAdapterTest {
 
+    private DrillsRvAdapter adapter;
+
     @Before
     public void setUp() {
         System.out.println("Running Set Up!");
+        adapter = new DrillsRvAdapter();
     }
 
     @Test
