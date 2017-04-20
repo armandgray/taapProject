49ba35b1a943d9e61a4c5737a81fed010f8c18b5
Armand Gray <armandgray@gmail.com>
2017-04-20 11:01:27 -0700

    added initial setup code to DrillsRvAdapterTest

--- TAAP/app/src/test/java/com/armandgray/taap/utils/DrillsRvAdapterTest.java
+++ TAAP/app/src/test/java/com/armandgray/taap/utils/DrillsRvAdapterTest.java
@@ -1,5 +1,33 @@
 package com.armandgray.taap.utils;
 
+import com.armandgray.taap.BuildConfig;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import static junit.framework.Assert.assertNotNull;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class)
 public class DrillsRvAdapterTest {
+
+    @Before
+    public void setUp() {
+        System.out.println("Running Set Up!");
+    }
+
+    @Test
+    public void itemCount() throws Exception {
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("Running TearDown!");
+    }
 
 }
