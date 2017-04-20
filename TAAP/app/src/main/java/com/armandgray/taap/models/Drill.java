8255ea1b42f0f083d645aac0752725b1d7b711a7
Armand Gray <armandgray@gmail.com>
2017-04-20 12:32:52 -0700

    created models.Drill and DrillTest

---
+++ TAAP/app/src/main/java/com/armandgray/taap/models/Drill.java
@@ -0,0 +1,4 @@
+package com.armandgray.taap.models;
+
+public class Drill {
+}
---
+++ TAAP/app/src/test/java/com/armandgray/taap/models/DrillTest.java
@@ -0,0 +1,12 @@
+package com.armandgray.taap.models;
+
+import org.junit.Test;
+
+public class DrillTest {
+
+    @Test
+    public void test() throws Exception {
+
+    }
+
+}
