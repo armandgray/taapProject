745bc82216f9a8e7817754d630ec53a19c46a2f3
Armand Gray <armandgray@gmail.com>
2017-04-20 19:13:05 -0700

    passed getDrillsList to DrillsAdapter in Views

--- TAAP/app/src/main/java/com/armandgray/taap/utils/DrillsHelper.java
+++ TAAP/app/src/main/java/com/armandgray/taap/utils/DrillsHelper.java
@@ -1,14 +1,15 @@
 package com.armandgray.taap.utils;
 
+import com.armandgray.taap.R;
 import com.armandgray.taap.models.Drill;
 
 import java.util.ArrayList;
 
 public class DrillsHelper {
 
-    private static ArrayList<Drill> drillsList;
-
     public static ArrayList<Drill> getDrillsList() {
+        ArrayList<Drill> drillsList = new ArrayList<>();
+        drillsList.add(new Drill("2-Ball Pound Dribble", R.drawable.ic_fitness_center_white_24dp));
         return drillsList;
     }
 }
--- TAAP/app/src/test/java/com/armandgray/taap/utils/DrillsHelperTest.java
+++ TAAP/app/src/test/java/com/armandgray/taap/utils/DrillsHelperTest.java
@@ -4,12 +4,14 @@ import org.junit.Test;
 
 import static com.armandgray.taap.utils.DrillsHelper.getDrillsList;
 import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertTrue;
 
 public class DrillsHelperTest {
 
     @Test
     public void canGetDrillsList() throws Exception {
         assertNotNull(getDrillsList());
+        assertTrue(getDrillsList().size() > 0);
     }
 
 
