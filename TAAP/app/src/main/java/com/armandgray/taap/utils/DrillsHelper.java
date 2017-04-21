f60594aefd312178b7c49032ccebfffdfaa71a9c
Armand Gray <armandgray@gmail.com>
2017-04-20 19:10:22 -0700

    created test

--- TAAP/app/src/main/java/com/armandgray/taap/utils/DrillsHelper.java
+++ TAAP/app/src/main/java/com/armandgray/taap/utils/DrillsHelper.java
@@ -1,4 +1,14 @@
 package com.armandgray.taap.utils;
 
+import com.armandgray.taap.models.Drill;
+
+import java.util.ArrayList;
+
 public class DrillsHelper {
+
+    private static ArrayList<Drill> drillsList;
+
+    public static ArrayList<Drill> getDrillsList() {
+        return drillsList;
+    }
 }
--- TAAP/app/src/test/java/com/armandgray/taap/utils/DrillsHelperTest.java
+++ TAAP/app/src/test/java/com/armandgray/taap/utils/DrillsHelperTest.java
@@ -1,5 +1,16 @@
 package com.armandgray.taap.utils;
 
+import org.junit.Test;
+
+import static com.armandgray.taap.utils.DrillsHelper.getDrillsList;
+import static junit.framework.Assert.assertNotNull;
+
 public class DrillsHelperTest {
+
+    @Test
+    public void canGetDrillsList() throws Exception {
+        assertNotNull(getDrillsList());
+    }
+
 
 }
