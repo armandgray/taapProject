ea66396443a962d809273b885cd124109996efd9
Armand Gray <armandgray@gmail.com>
2017-04-22 16:07:51 -0700

    added failing code to canGetQueryResultList

--- TAAP/app/src/test/java/com/armandgray/taap/models/DrillTest.java
+++ TAAP/app/src/test/java/com/armandgray/taap/models/DrillTest.java
@@ -4,12 +4,15 @@ import com.armandgray.taap.R;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.armandgray.taap.models.Drill.BALL_HANDLING;
 import static com.armandgray.taap.models.Drill.getQueryResultList;
 import static com.armandgray.taap.utils.DrillsHelper.getDrillsList;
+import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertTrue;
 
 public class DrillTest {
 
@@ -26,7 +29,19 @@ public class DrillTest {
 
     @Test
     public void canGetQueryResultList() throws Exception {
+        ArrayList<Drill> expectedList = getDrillsList();
+        for (int i = 0; i < expectedList.size(); i++) {
+            if (!expectedList.get(i).getTitle().contains("pro")) {
+                expectedList.remove(i);
+                i--;
+            }
+        }
+
         List<Drill> returnList = getQueryResultList(getDrillsList(), "pro");
         assertNotNull(returnList);
+        assertEquals(expectedList.size(), returnList.size());
+        for (int i = 0; i < expectedList.size(); i++) {
+            assertTrue(expectedList.get(i).getTitle().equals(returnList.get(i).getTitle()));
+        }
     }
 }
