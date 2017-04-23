42f296fe7e31c37a60c351d092a7791d5565de66
Armand Gray <armandgray@gmail.com>
2017-04-23 11:04:33 -0700

    added test doesPassViewFromTouchListenerToItemClickInterfaceMethod

--- TAAP/app/src/test/java/com/armandgray/taap/utils/RecyclerItemClickListenerTest.java
+++ TAAP/app/src/test/java/com/armandgray/taap/utils/RecyclerItemClickListenerTest.java
@@ -2,17 +2,23 @@ package com.armandgray.taap.utils;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.MotionEvent;
 import android.view.View;
 
 import org.junit.Test;
 import org.mockito.Mock;
 
 import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertTrue;
 
 public class RecyclerItemClickListenerTest {
 
     @Mock
     Context context;
+    RecyclerView recyclerView;
+    MotionEvent motionEvent;
+    private View resultView;
+    private int resultPosition = -1;
 
     @Test
     public void canCreateRecyclerItemClickListener_TestConstructor() throws Exception {
@@ -42,6 +48,22 @@ public class RecyclerItemClickListenerTest {
                     public void onItemClick(View view, int position) {}
                 });
         assertNotNull(clickListener);
+    }
+
+    @Test
+    public void doesPassViewFromTouchListenerToItemClickInterfaceMethod() throws Exception {
+        RecyclerItemClickListener clickListener = new RecyclerItemClickListener(context,
+                new RecyclerItemClickListener.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(View view, int position) {
+                        resultView = view;
+                        resultPosition = position;
+                    }
+                });
+        clickListener.onInterceptTouchEvent(recyclerView, motionEvent);
+
+        assertNotNull(resultView);
+        assertTrue(resultPosition != -1);
     }
 
 }
