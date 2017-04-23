1130e038fb5f6e6bc231266719f00b924f0df644
Armand Gray <armandgray@gmail.com>
2017-04-23 10:42:38 -0700

    added passed params to canCreateRecyclerItemClickListener_TestConstructor constructor

--- TAAP/app/src/test/java/com/armandgray/taap/utils/RecyclerItemClickListenerTest.java
+++ TAAP/app/src/test/java/com/armandgray/taap/utils/RecyclerItemClickListenerTest.java
@@ -1,14 +1,21 @@
 package com.armandgray.taap.utils;
 
+import android.content.Context;
+
 import org.junit.Test;
+import org.mockito.Mock;
 
 import static junit.framework.Assert.assertNotNull;
 
 public class RecyclerItemClickListenerTest {
 
+    @Mock
+    Context context;
+
     @Test
-    public void canCreateRecyclerItemClickListener() throws Exception {
-        assertNotNull(new RecyclerItemClickListener());
+    public void canCreateRecyclerItemClickListener_TestConstructor() throws Exception {
+        assertNotNull(new RecyclerItemClickListener(context,
+                new RecyclerItemClickListener.OnItemClickListener() {}));
     }
 
 }
