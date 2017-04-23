d255eb35559cd773e634b1075f8bbb3a4445c439
Armand Gray <armandgray@gmail.com>
2017-04-23 10:51:06 -0700

    added passing gestureDetector

--- TAAP/app/src/main/java/com/armandgray/taap/utils/RecyclerItemClickListener.java
+++ TAAP/app/src/main/java/com/armandgray/taap/utils/RecyclerItemClickListener.java
@@ -3,6 +3,7 @@ package com.armandgray.taap.utils;
 import android.content.Context;
 import android.support.annotation.VisibleForTesting;
 import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class RecyclerItemClickListener {
@@ -12,6 +13,12 @@ public class RecyclerItemClickListener {
 
     public RecyclerItemClickListener(Context context, OnItemClickListener listener) {
         this.listener = listener;
+        this.gestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onSingleTapUp(MotionEvent e) {
+                return true;
+            }
+        });
     }
 
     public interface OnItemClickListener {
