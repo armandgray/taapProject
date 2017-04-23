1a2c2d343e5c6f10219885d6ff20c9772ea8b71f
Armand Gray <armandgray@gmail.com>
2017-04-23 10:44:26 -0700

    added passing constructor and interface

--- TAAP/app/src/main/java/com/armandgray/taap/utils/RecyclerItemClickListener.java
+++ TAAP/app/src/main/java/com/armandgray/taap/utils/RecyclerItemClickListener.java
@@ -1,5 +1,15 @@
 package com.armandgray.taap.utils;
 
+import android.content.Context;
+
 public class RecyclerItemClickListener {
 
+    private final OnItemClickListener listener;
+
+    public RecyclerItemClickListener(Context context, OnItemClickListener listener) {
+        this.listener = listener;
+    }
+
+    public interface OnItemClickListener {
+    }
 }
