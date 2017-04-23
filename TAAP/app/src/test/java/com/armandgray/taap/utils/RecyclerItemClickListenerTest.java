c20d6bdc027fb7286aa30c31bd93c794fa3ba3e5
Armand Gray <armandgray@gmail.com>
2017-04-23 10:38:58 -0700

    Added test canCreateRecyclerItemClickListener

--- TAAP/app/src/test/java/com/armandgray/taap/utils/RecyclerItemClickListenerTest.java
+++ TAAP/app/src/test/java/com/armandgray/taap/utils/RecyclerItemClickListenerTest.java
@@ -1,7 +1,14 @@
 package com.armandgray.taap.utils;
 
+import org.junit.Test;
+
+import static junit.framework.Assert.assertNotNull;
+
 public class RecyclerItemClickListenerTest {
 
-    
+    @Test
+    public void canCreateRecyclerItemClickListener() throws Exception {
+        assertNotNull(new RecyclerItemClickListener());
+    }
 
 }
