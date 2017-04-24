cd13c63713bab7804ed3f806fc5d1d3ce0f4b427
Armand Gray <armandgray@gmail.com>
2017-04-24 11:53:57 -0700

    refactored activity code into Views

--- TAAP/app/src/main/java/com/armandgray/taap/detail/DrillDetailActivity.java
+++ TAAP/app/src/main/java/com/armandgray/taap/detail/DrillDetailActivity.java
@@ -1,13 +1,7 @@
 package com.armandgray.taap.detail;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-
-import com.armandgray.taap.R;
 
 public class DrillDetailActivity extends AppCompatActivity {
 
@@ -18,18 +12,6 @@ public class DrillDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         controller = new DrillDetailController(this);
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
--- TAAP/app/src/main/java/com/armandgray/taap/detail/DrillDetailViews.java
+++ TAAP/app/src/main/java/com/armandgray/taap/detail/DrillDetailViews.java
@@ -1,10 +1,14 @@
 package com.armandgray.taap.detail;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.widget.Toolbar;
+
 import com.armandgray.taap.R;
 
 class DrillDetailViews {
 
     public DrillDetailActivity activity;
+    private FloatingActionButton fab;
 
     DrillDetailViews(DrillDetailActivity activity) {
         this.activity = activity;
@@ -13,7 +17,18 @@ class DrillDetailViews {
     }
 
     private void setupActivityInitialState() {
-       activity.setContentView(R.layout.activity_drill_detail);
-
+        activity.setContentView(R.layout.activity_drill_detail);
+        assignGlobalViews();
+        setupToolbar();
     }
+
+    private void assignGlobalViews() {
+        fab = (FloatingActionButton) activity.findViewById(R.id.fab);
+    }
+
+    private void setupToolbar() {
+        Toolbar toolbar = (Toolbar) activity.findViewById(R.id.toolbar);
+        activity.setSupportActionBar(toolbar);
+    }
+
 }
