0efd8a254e794a37dfcdd2b9cb6ac61c12274ccf
Armand Gray <armandgray@gmail.com>
2017-04-24 08:53:43 -0700

    added DrillDetailViewsTest

---
+++ TAAP/app/src/test/java/com/armandgray/taap/detail/DrillDetailViewsTest.java
@@ -0,0 +1,87 @@
+package com.armandgray.taap.detail;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+
+import com.armandgray.taap.R;
+import com.armandgray.taap.settings.SettingsActivity;
+import com.armandgray.taap.settings.SettingsActivityViews;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.robolectric.Robolectric;
+import org.robolectric.android.controller.ActivityController;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertTrue;
+import static org.robolectric.Shadows.shadowOf;
+
+public class DrillDetailViewsTest {
+
+    private ActivityController<DrillDetailActivity> activityController;
+    private DrillDetailActivity activity;
+    private Toolbar toolbar;
+    private DrillDetailViews views;
+
+    @Before
+    public void setUp() {
+        System.out.println("Running Set Up!");
+        activityController = Robolectric.buildActivity(DrillDetailActivity.class);
+        activity = activityController.create().visible().get();
+        toolbar = (Toolbar) activity.findViewById(R.id.toolbar);
+        views = activity.controller.views;
+    }
+
+    @Test
+    public void activityInstanceOfMainActivity_TestConstructor() throws Exception {
+        assertEquals("settings.SettingsActivity", views.activity.getLocalClassName());
+    }
+
+    @Test
+    public void doesSetContentView_MethodTest_SetupActivityInitialState() throws Exception {
+        assertEquals(R.id.activitySettingsLayout, shadowOf(activity).getContentView().getId());
+    }
+
+    @Test
+    public void doesSetHomeAsUpEnabled_MethodTest_SetupActivityInitialState() throws Exception {
+        assertNotNull(activity.getSupportActionBar());
+        final int displayOptions = activity.getSupportActionBar().getDisplayOptions();
+        assertTrue((displayOptions & ActionBar.DISPLAY_SHOW_HOME) != 0);
+        assertTrue((displayOptions & ActionBar.DISPLAY_HOME_AS_UP) != 0);
+    }
+
+    @Test
+    public void doesSetupHideToolbarTitle_MethodTest_SetupActivityInitialState() throws Exception {
+        ActionBar actionBar = activity.getSupportActionBar();
+        assertNotNull(actionBar);
+        final int displayOptions = activity.getSupportActionBar().getDisplayOptions();
+        assertTrue((displayOptions & ActionBar.DISPLAY_SHOW_TITLE) == 0);
+    }
+
+    @Test
+    public void hasCustomToolbarTitle() throws Exception {
+        Toolbar toolbar = (Toolbar) activity.findViewById(R.id.toolbar);
+        TextView tvTitle = (TextView) toolbar.findViewById(R.id.tvTitle);
+        assertNotNull(tvTitle);
+    }
+
+    @Test
+    public void doesSetCustomToolbarTitleText_MethodTest_SetupActivityInitialState() throws Exception {
+        Toolbar toolbar = (Toolbar) activity.findViewById(R.id.toolbar);
+        TextView tvTitle = (TextView) toolbar.findViewById(R.id.tvTitle);
+        assertEquals("Settings", tvTitle.getText());
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("Running TearDown!");
+        activityController.pause().stop().destroy();
+        activity = null;
+        toolbar = null;
+        views = null;
+    }
+
+}
