7ad7237fe2f8b52e0cedd19ece382ba2f101264d
Armand Gray <armandgray@gmail.com>
2017-04-24 08:45:23 -0700

    added content to DrillControllerTest

--- TAAP/app/src/test/java/com/armandgray/taap/detail/DrillDetailControllerTest.java
+++ TAAP/app/src/test/java/com/armandgray/taap/detail/DrillDetailControllerTest.java
@@ -1,8 +1,53 @@
 package com.armandgray.taap.detail;
 
-/**
- * Created by armandgray on 4/24/17.
- */
+import com.armandgray.taap.BuildConfig;
+import com.armandgray.taap.detail.DrillDetailActivity;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.android.controller.ActivityController;
+import org.robolectric.annotation.Config;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class)
 public class DrillDetailControllerTest {
+
+    private ActivityController<DrillDetailActivity> activityController;
+    private DrillDetailActivity activity;
+    private DrillDetailController controller;
+
+    @Before
+    public void setUp() {
+        System.out.println("Running Set Up!");
+        activityController = Robolectric.buildActivity(DrillDetailActivity.class);
+        activity = activityController.create().visible().get();
+        controller = activity.controller;
+    }
+
+    @Test
+    public void activityInstanceOfAppCompatActivity_TestConstructor() throws Exception {
+        assertEquals("detail.DrillDetailActivity", controller.activity.getLocalClassName());
+    }
+
+    @Test
+    public void doesCreateViewsHandler_TestConstructor() throws Exception {
+        assertNotNull(controller.views);
+        assertNotNull(controller.views.activity);
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("Running TearDown!");
+        activityController.pause().stop().destroy();
+        activity = null;
+        controller = null;
+    }
 
 }
