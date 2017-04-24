23f5e8b0a97c31916ea5e966156cfe855eeb45ca
Armand Gray <armandgray@gmail.com>
2017-04-24 12:23:19 -0700

    Added hasView_IvImage

--- TAAP/app/src/test/java/com/armandgray/taap/detail/DrillDetailResTest.java
+++ TAAP/app/src/test/java/com/armandgray/taap/detail/DrillDetailResTest.java
@@ -34,6 +34,36 @@ public class DrillDetailResTest {
         activity = activityController.create().visible().get();
     }
 
+    @Test
+    public void hasLayout_AboutContainer() throws Exception {
+        assertNotNull(View.inflate(activity, R.layout.content_settings, null));
+    }
+
+    @Test
+    public void hasView_AboutContainer_FirstTopBorder() throws Exception {
+        LinearLayout container = (LinearLayout) View.inflate(activity, R.layout.content_settings, null);
+        assertNotNull(container.findViewById(R.id.firstTopBorder));
+    }
+
+    @Test
+    public void hasView_AboutContainer_TvRateThisApp() throws Exception {
+        LinearLayout container = (LinearLayout) View.inflate(activity, R.layout.content_settings, null);
+        assertNotNull(container.findViewById(R.id.tvRateThisApp));
+    }
+
+    @Test
+    public void hasDrawable_AboutTextBackground() throws Exception {
+        LinearLayout container = (LinearLayout) View.inflate(activity, R.layout.content_settings, null);
+        TextView textView = (TextView) container.findViewById(R.id.tvRateThisApp);
+        assertEquals(activity.getResources().getDrawable(R.drawable.about_item_background), textView.getBackground());
+    }
+
+    @Test
+    public void hasView_IvImage() throws Exception {
+        LinearLayout container = (LinearLayout) View.inflate(activity, R.layout.content_drill_detail, null);
+        assertNotNull(container.findViewById(R.id.ivImage));
+    }
+
     @After
     public void tearDown() {
         System.out.println("Running TearDown!");
