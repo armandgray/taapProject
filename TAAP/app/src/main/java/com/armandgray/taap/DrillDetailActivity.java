b99637b076d33603d104cd0b58987cd40f38d83d
Armand Gray <armandgray@gmail.com>
2017-04-24 08:41:28 -0700

    added test class DrillDetailActivityTest

--- TAAP/app/src/main/java/com/armandgray/taap/DrillDetailActivity.java
+++ TAAP/app/src/main/java/com/armandgray/taap/DrillDetailActivity.java
@@ -9,6 +9,8 @@ import android.view.View;
 
 public class DrillDetailActivity extends AppCompatActivity {
 
+    public String controller;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
