7d851f7e10b6dffb22a256e30cc9912334547a1d
Armand Gray <armandgray@gmail.com>
2017-04-24 12:34:44 -0700

    added passing view ivimage

--- TAAP/app/src/test/java/com/armandgray/taap/detail/DrillDetailResTest.java
+++ TAAP/app/src/test/java/com/armandgray/taap/detail/DrillDetailResTest.java
@@ -35,30 +35,6 @@ public class DrillDetailResTest {
     }
 
     @Test
-    public void hasLayout_AboutContainer() throws Exception {
-        assertNotNull(View.inflate(activity, R.layout.content_settings, null));
-    }
-
-    @Test
-    public void hasView_AboutContainer_FirstTopBorder() throws Exception {
-        LinearLayout container = (LinearLayout) View.inflate(activity, R.layout.content_settings, null);
-        assertNotNull(container.findViewById(R.id.firstTopBorder));
-    }
-
-    @Test
-    public void hasView_AboutContainer_TvRateThisApp() throws Exception {
-        LinearLayout container = (LinearLayout) View.inflate(activity, R.layout.content_settings, null);
-        assertNotNull(container.findViewById(R.id.tvRateThisApp));
-    }
-
-    @Test
-    public void hasDrawable_AboutTextBackground() throws Exception {
-        LinearLayout container = (LinearLayout) View.inflate(activity, R.layout.content_settings, null);
-        TextView textView = (TextView) container.findViewById(R.id.tvRateThisApp);
-        assertEquals(activity.getResources().getDrawable(R.drawable.about_item_background), textView.getBackground());
-    }
-
-    @Test
     public void hasView_IvImage() throws Exception {
         LinearLayout container = (LinearLayout) View.inflate(activity, R.layout.content_drill_detail, null);
         assertNotNull(container.findViewById(R.id.ivImage));
