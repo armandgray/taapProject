5b30c159d82a731bd0ad3e9eda6d6b4732079e9d
Armand Gray <armandgray@gmail.com>
2017-04-24 23:24:18 -0700

    Added passing method setupRvSummary with DrillAdapter

--- TAAP/app/src/main/java/com/armandgray/taap/detail/DetailSummaryDialog.java
+++ TAAP/app/src/main/java/com/armandgray/taap/detail/DetailSummaryDialog.java
@@ -7,15 +7,21 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 
 import com.armandgray.taap.LogActivity;
 import com.armandgray.taap.R;
+import com.armandgray.taap.utils.DrillsRvAdapter;
+
+import static com.armandgray.taap.utils.DrillsHelper.getDrillsList;
 
 class DetailSummaryDialog {
 
     Activity activity;
+    private RecyclerView rvSummary;
 
     public DetailSummaryDialog(Activity activity) {
         this.activity = activity;
@@ -25,6 +31,7 @@ class DetailSummaryDialog {
         AlertDialog.Builder builder = new AlertDialog.Builder(activity);
         @SuppressLint("InflateParams") View dialogLayout = LayoutInflater.from(activity)
                 .inflate(R.layout.detail_summary_dialog_layout, null);
+        rvSummary = (RecyclerView) dialogLayout.findViewById(R.id.rvSummary);
         builder.setView(dialogLayout)
                 .setNeutralButton("Continue", new DialogInterface.OnClickListener() {
                     @Override
@@ -33,6 +40,13 @@ class DetailSummaryDialog {
                         activity.startActivity(intent);
                     }
                 });
+        setupRvSummary();
         return builder.create();
+    }
+
+    private void setupRvSummary() {
+        rvSummary.setAdapter(new DrillsRvAdapter(getDrillsList()));
+        rvSummary.setLayoutManager(
+                new LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false));
     }
 }
