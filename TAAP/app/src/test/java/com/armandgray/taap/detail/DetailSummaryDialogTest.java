2be4759ce951152cfe990de47ae9c95ad6a5de1e
Armand Gray <armandgray@gmail.com>
2017-04-24 20:52:42 -0700

    Added method canCreateDetailSummaryDialog_TestOnCreateDialog

--- TAAP/app/src/test/java/com/armandgray/taap/detail/DetailSummaryDialogTest.java
+++ TAAP/app/src/test/java/com/armandgray/taap/detail/DetailSummaryDialogTest.java
@@ -1,5 +1,17 @@
 package com.armandgray.taap.detail;
 
+import android.os.Bundle;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertNotNull;
+
 public class DetailSummaryDialogTest {
+
+    @Test
+    public void canCreateDetailSummaryDialog_TestOnCreateDialog() {
+        Bundle savedInstanceState = new Bundle();
+        assertNotNull(DetailSummaryDialog.onCreateDialog(savedInstanceState));
+    }
 
 }
