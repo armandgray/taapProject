30c48e1a1147fc319d94e5fa5dce109d17e1c3df
Armand Gray <armandgray@gmail.com>
2017-04-25 16:45:52 -0700

    Added test canCreateSessionLogWithBuilder

--- TAAP/app/src/main/java/com/armandgray/taap/models/SessionLog.java
+++ TAAP/app/src/main/java/com/armandgray/taap/models/SessionLog.java
@@ -1,4 +1,12 @@
 package com.armandgray.taap.models;
 
 public class SessionLog {
+
+    public static class Builder {
+
+        public SessionLog create() {
+            return null;
+        }
+    }
+
 }
--- TAAP/app/src/test/java/com/armandgray/taap/models/SessionLogTest.java
+++ TAAP/app/src/test/java/com/armandgray/taap/models/SessionLogTest.java
@@ -1,5 +1,15 @@
 package com.armandgray.taap.models;
 
+import org.junit.Test;
+
+import static junit.framework.Assert.assertNotNull;
+
 public class SessionLogTest {
+
+    @Test
+    public void canCreateSessionLogWithBuilder() throws Exception {
+        SessionLog sessionLog = new SessionLog.Builder().create();
+        assertNotNull(sessionLog);
+    }
 
 }
