4833d7225b6b30cf1b38cdcc8d1d5412f1fa22d7
Armand Gray <armandgray@gmail.com>
2017-05-02 17:10:41 -0700

    Added canCreateSessionLogWithDefaultValues

--- TAAP/app/src/test/java/com/armandgray/taap/models/SessionLogTest.java
+++ TAAP/app/src/test/java/com/armandgray/taap/models/SessionLogTest.java
@@ -14,15 +14,30 @@ public class SessionLogTest {
     @Test
     public void canCreateSessionLogWithBuilder() throws Exception {
         SessionLog sessionLog = new SessionLog.Builder()
-                .sessionLength(new Date(0))
-                .sessionGoal(new Date(0))
-                .activeWork(new Date(0))
-                .restTime(new Date(0))
-                .setsCompleted(0)
-                .repsCompleted(0)
-                .successRate(0.0)
-                .successRecord(0.0)
+                .sessionLength(new Date(1))
+                .sessionGoal(new Date(3))
+                .activeWork(new Date(4))
+                .restTime(new Date(5))
+                .setsCompleted(4)
+                .repsCompleted(3)
+                .successRate(0.23)
+                .successRecord(0.55)
                 .create();
+        assertNotNull(sessionLog);
+        assertNotNull(sessionLog.getSessionDate());
+        assertNotNull(sessionLog.getSessionLength());
+        assertNotNull(sessionLog.getSessionGoal());
+        assertNotNull(sessionLog.getActiveWork());
+        assertNotNull(sessionLog.getRestTime());
+        assertNotNull(sessionLog.getSetsCompleted());
+        assertNotNull(sessionLog.getRepsCompleted());
+        assertNotNull(sessionLog.getSuccessRate());
+        assertNotNull(sessionLog.getSuccessRecord());
+    }
+
+    @Test
+    public void canCreateSessionLogWithDefaultValues() throws Exception {
+        SessionLog sessionLog = new SessionLog.Builder().create();
         assertNotNull(sessionLog);
         assertNotNull(sessionLog.getSessionDate());
         assertNotNull(sessionLog.getSessionLength());
