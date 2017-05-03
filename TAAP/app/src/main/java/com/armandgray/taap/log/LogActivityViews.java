8845414e3d1d7573a07a6ae83f4780bb224c9f13
Armand Gray <armandgray@gmail.com>
2017-05-03 12:14:32 -0700

    added passing setup code for toolbar

--- TAAP/app/src/main/java/com/armandgray/taap/log/LogActivityViews.java
+++ TAAP/app/src/main/java/com/armandgray/taap/log/LogActivityViews.java
@@ -1,13 +1,14 @@
 package com.armandgray.taap.log;
 
 import android.support.annotation.VisibleForTesting;
+import android.support.v7.widget.Toolbar;
 
 import com.armandgray.taap.R;
 
 class LogActivityViews {
 
     @VisibleForTesting LogActivity activity;
-    @VisibleForTesting LogViewsListener listener;
+    private LogViewsListener listener;
 
     LogActivityViews(LogActivity activity, LogViewsListener listener) {
         this.activity = activity;
@@ -16,6 +17,16 @@ class LogActivityViews {
 
     void setupActivityInitialState() {
         activity.setContentView(R.layout.activity_log);
+        setupToolbar();
+    }
+
+    private void setupToolbar() {
+        Toolbar toolbar = (Toolbar) activity.findViewById(R.id.toolbar);
+        activity.setSupportActionBar(toolbar);
+
+        if (activity.getSupportActionBar() != null) {
+            activity.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
     }
 
     interface LogViewsListener {
