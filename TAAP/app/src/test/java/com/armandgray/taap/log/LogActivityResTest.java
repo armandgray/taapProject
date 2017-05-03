8c1983d8342ee07155cafc351c1c100c19bf3cbb
Armand Gray <armandgray@gmail.com>
2017-05-03 12:39:52 -0700

    added test hasLayout_DetailContainer

--- TAAP/app/src/test/java/com/armandgray/taap/log/LogActivityResTest.java
+++ TAAP/app/src/test/java/com/armandgray/taap/log/LogActivityResTest.java
@@ -1,14 +1,21 @@
 package com.armandgray.taap.log;
 
+import android.view.View;
+import android.widget.LinearLayout;
+
 import com.armandgray.taap.BuildConfig;
+import com.armandgray.taap.R;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.android.controller.ActivityController;
 import org.robolectric.annotation.Config;
+
+import static junit.framework.Assert.assertNotNull;
 
 @RunWith(RobolectricTestRunner.class)
 @Config(constants = BuildConfig.class)
@@ -24,6 +31,11 @@ public class LogActivityResTest {
         activity = activityController.create().visible().get();
     }
 
+    @Test
+    public void hasLayout_DetailContainer() throws Exception {
+        LinearLayout container = (LinearLayout) View.inflate(activity, R.layout.content_log, null);
+        assertNotNull(container.findViewById(R.id.detailContainer));
+    }
 
     @After
     public void tearDown() {
