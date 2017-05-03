0cf62097ea370b53e8a0f2b871c53b8ca8db4f1e
Armand Gray <armandgray@gmail.com>
2017-05-03 12:10:11 -0700

    removed fab?

--- TAAP/app/src/main/java/com/armandgray/taap/log/LogActivity.java
+++ TAAP/app/src/main/java/com/armandgray/taap/log/LogActivity.java
@@ -2,11 +2,8 @@ package com.armandgray.taap.log;
 
 import android.os.Bundle;
 import android.support.annotation.VisibleForTesting;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import com.armandgray.taap.R;
 
@@ -23,14 +20,6 @@ public class LogActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
