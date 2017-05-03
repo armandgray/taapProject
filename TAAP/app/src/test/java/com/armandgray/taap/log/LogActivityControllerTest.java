ccf7725a3871c9b688a987c9c91e390df0ea340c
Armand Gray <armandgray@gmail.com>
2017-05-03 08:43:06 -0700

    created ControllerTest with Robo

---
+++ TAAP/app/src/test/java/com/armandgray/taap/log/LogActivityControllerTest.java
@@ -0,0 +1,38 @@
+package com.armandgray.taap.log;
+
+import com.armandgray.taap.BuildConfig;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.android.controller.ActivityController;
+import org.robolectric.annotation.Config;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class)
+public class LogActivityControllerTest {
+
+    private static final String W_ALL = "wAll";
+    private ActivityController<LogActivity> activityController;
+    private LogActivity activity;
+    private LogActivityController controller;
+
+    @Before
+    public void setUp() {
+        System.out.println("Running Set Up!");
+        activityController = Robolectric.buildActivity(LogActivity.class);
+        activity = activityController.create().visible().get();
+        controller = activity.controller;
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("Running TearDown!");
+        activityController.pause().stop().destroy();
+        activity = null;
+        controller = null;
+    }
+
+}
