115ed74fb5cc1a4adbc7debabf5ae2b554a549ab
Armand Gray <armandgray@gmail.com>
2017-05-04 09:54:30 -0700

    added robo to dbHelper Test

--- TAAP/app/src/test/java/com/armandgray/taap/db/DatabaseOpenHelperTest.java
+++ TAAP/app/src/test/java/com/armandgray/taap/db/DatabaseOpenHelperTest.java
@@ -2,10 +2,17 @@ package com.armandgray.taap.db;
 
 import android.database.sqlite.SQLiteOpenHelper;
 
+import com.armandgray.taap.BuildConfig;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
 import static junit.framework.Assert.assertNotNull;
 
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class)
 public class DatabaseOpenHelperTest {
 
     @Test
