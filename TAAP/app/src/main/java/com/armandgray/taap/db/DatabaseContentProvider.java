09e581cdd8353813154cb674be7a9b3306fb1278
Armand Gray <armandgray@gmail.com>
2017-05-04 10:51:24 -0700

    added passing call to Uri.parse for URI_DRILLS && robo

--- TAAP/app/src/main/java/com/armandgray/taap/db/DatabaseContentProvider.java
+++ TAAP/app/src/main/java/com/armandgray/taap/db/DatabaseContentProvider.java
@@ -1,10 +1,11 @@
 package com.armandgray.taap.db;
 
+import android.net.Uri;
 import android.support.annotation.VisibleForTesting;
 
 public class DatabaseContentProvider {
 
     @VisibleForTesting static final String AUTHORITY = "com.armandgray.seeme.db.notesprovider";
     @VisibleForTesting static final String BASE_PATH_DRILLS = "drills";
-    public static final Object CONTENT_URI_DRILLS = null;
+    public static final Uri CONTENT_URI_DRILLS = Uri.parse("content://" + AUTHORITY + "/" + BASE_PATH_DRILLS);
 }
--- TAAP/app/src/test/java/com/armandgray/taap/db/DatabaseContentProviderTest.java
+++ TAAP/app/src/test/java/com/armandgray/taap/db/DatabaseContentProviderTest.java
@@ -2,7 +2,12 @@ package com.armandgray.taap.db;
 
 import android.net.Uri;
 
+import com.armandgray.taap.BuildConfig;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
 import static com.armandgray.taap.db.DatabaseContentProvider.AUTHORITY;
 import static com.armandgray.taap.db.DatabaseContentProvider.BASE_PATH_DRILLS;
@@ -10,6 +15,8 @@ import static com.armandgray.taap.db.DatabaseContentProvider.CONTENT_URI_DRILLS;
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotNull;
 
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class)
 public class DatabaseContentProviderTest {
 
     @Test
