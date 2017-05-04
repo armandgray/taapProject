ddf6cc8386f5277e943de69505d202d2c4882ec6
Armand Gray <armandgray@gmail.com>
2017-05-04 12:59:52 -0700

    added expected code to constant passing test

--- TAAP/app/src/main/java/com/armandgray/taap/db/DrillsTable.java
+++ TAAP/app/src/main/java/com/armandgray/taap/db/DrillsTable.java
@@ -1,12 +1,18 @@
 package com.armandgray.taap.db;
 
 class DrillsTable {
-    
+
     static final String TABLE_DRILLS = "drills";
     static final String COLUMN_ID = "drillId";
     static final String COLUMN_TITLE = "itemTitle";
     static final String COLUMN_IMAGE_ID = "itemImageId";
     static final String COLUMN_CATEGORY = "itemCategory";
 
-    static final String SQL_CREATE = null;
+    static final String SQL_CREATE =
+            "CREATE TABLE " + TABLE_DRILLS + " ("
+                    + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + COLUMN_TITLE + " TEXT, "
+                    + COLUMN_IMAGE_ID + " INTEGER,"
+                    + COLUMN_CATEGORY + " TEXT"
+                    + ")";
 }
