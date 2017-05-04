e720474dacfea0fc16e589dd2f40e3490853d204
Armand Gray <armandgray@gmail.com>
2017-05-04 11:37:32 -0700

    Added passing assertEquals to hasFields_TableColumns

--- TAAP/app/src/test/java/com/armandgray/taap/db/DrillsTableTest.java
+++ TAAP/app/src/test/java/com/armandgray/taap/db/DrillsTableTest.java
@@ -7,6 +7,7 @@ import static com.armandgray.taap.db.DrillsTable.COLUMN_ID;
 import static com.armandgray.taap.db.DrillsTable.COLUMN_IMAGE_ID;
 import static com.armandgray.taap.db.DrillsTable.COLUMN_TITLE;
 import static com.armandgray.taap.db.DrillsTable.TABLE_DRILLS;
+import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotNull;
 
 public class DrillsTableTest {
@@ -14,10 +15,15 @@ public class DrillsTableTest {
     @Test
     public void hasFields_TableColumns() throws Exception {
         assertNotNull(TABLE_DRILLS);
+        assertEquals("drills", TABLE_DRILLS);
         assertNotNull(COLUMN_ID);
+        assertEquals("drillId", COLUMN_ID);
         assertNotNull(COLUMN_TITLE);
+        assertEquals("itemTitle", COLUMN_TITLE);
         assertNotNull(COLUMN_IMAGE_ID);
+        assertEquals("itemImageId", COLUMN_IMAGE_ID);
         assertNotNull(COLUMN_CATEGORY);
+        assertEquals("itemCategory", COLUMN_CATEGORY);
     }
 
 }
