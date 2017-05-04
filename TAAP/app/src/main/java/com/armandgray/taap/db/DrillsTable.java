ca3897d6f85dd6769e5e5f2d7cf3a69ea7ba88bb
Armand Gray <armandgray@gmail.com>
2017-05-04 11:34:02 -0700

    Added test failing hasFields_TableColumns

--- TAAP/app/src/main/java/com/armandgray/taap/db/DrillsTable.java
+++ TAAP/app/src/main/java/com/armandgray/taap/db/DrillsTable.java
@@ -1,5 +1,10 @@
 package com.armandgray.taap.db;
 
 public class DrillsTable {
-
+    
+    static final String TABLE_DRILLS = null;
+    static final String COLUMN_ID = null;
+    static final String COLUMN_TITLE = null;
+    static final String COLUMN_IMAGE_ID = null;
+    static final String COLUMN_CATEGORY = null;
 }
--- TAAP/app/src/test/java/com/armandgray/taap/db/DrillsTableTest.java
+++ TAAP/app/src/test/java/com/armandgray/taap/db/DrillsTableTest.java
@@ -1,6 +1,23 @@
 package com.armandgray.taap.db;
 
+import org.junit.Test;
+
+import static com.armandgray.taap.db.DrillsTable.COLUMN_CATEGORY;
+import static com.armandgray.taap.db.DrillsTable.COLUMN_ID;
+import static com.armandgray.taap.db.DrillsTable.COLUMN_IMAGE_ID;
+import static com.armandgray.taap.db.DrillsTable.COLUMN_TITLE;
+import static com.armandgray.taap.db.DrillsTable.TABLE_DRILLS;
+import static junit.framework.Assert.assertNotNull;
+
 public class DrillsTableTest {
 
+    @Test
+    public void hasFields_TableColumns() throws Exception {
+        assertNotNull(TABLE_DRILLS);
+        assertNotNull(COLUMN_ID);
+        assertNotNull(COLUMN_TITLE);
+        assertNotNull(COLUMN_IMAGE_ID);
+        assertNotNull(COLUMN_CATEGORY);
+    }
 
 }
