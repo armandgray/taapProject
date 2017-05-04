09a31188b224b74462c78c67bd1920622d64c1d4
Armand Gray <armandgray@gmail.com>
2017-05-04 08:46:36 -0700

    Added test doesImplementSQLiteOpenHelper

--- TAAP/app/src/test/java/com/armandgray/taap/db/DatabaseOpenHelperTest.java
+++ TAAP/app/src/test/java/com/armandgray/taap/db/DatabaseOpenHelperTest.java
@@ -1,7 +1,16 @@
 package com.armandgray.taap.db;
 
-import static org.junit.Assert.*;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertNotNull;
 
 public class DatabaseOpenHelperTest {
 
+    @Test
+    public void doesImplementSQLiteOpenHelper() throws Exception {
+        SQLiteOpenHelper databaseOpenHelper = new DatabaseOpenHelper();
+        assertNotNull(databaseOpenHelper);
+    }
 }
