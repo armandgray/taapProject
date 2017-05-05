eafb5aa52bcbd9cdba3a77c482c83cc904731cbb
Armand Gray <armandgray@gmail.com>
2017-05-04 22:19:21 -0700

    added passing code to ALL_COLUMNS constant

--- TAAP/app/src/main/java/com/armandgray/taap/db/DrillsTable.java
+++ TAAP/app/src/main/java/com/armandgray/taap/db/DrillsTable.java
@@ -7,7 +7,8 @@ class DrillsTable {
     static final String COLUMN_TITLE = "itemTitle";
     static final String COLUMN_IMAGE_ID = "itemImageId";
     static final String COLUMN_CATEGORY = "itemCategory";
-    static final String[] ALL_COLUMNS = {};
+    static final String[] ALL_COLUMNS = {
+            COLUMN_ID, COLUMN_TITLE, COLUMN_IMAGE_ID, COLUMN_CATEGORY };
 
     static final String SQL_CREATE =
             "CREATE TABLE " + TABLE_DRILLS + " ("
