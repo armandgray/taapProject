2950b965928931dcf2706ba196a0796763f48f7b
Armand Gray <armandgray@gmail.com>
2017-05-04 22:41:35 -0700

    copied sqlCreate code to field

--- TAAP/app/src/main/java/com/armandgray/taap/db/LogsTable.java
+++ TAAP/app/src/main/java/com/armandgray/taap/db/LogsTable.java
@@ -1,5 +1,8 @@
 package com.armandgray.taap.db;
 
+import static com.armandgray.taap.db.DrillsTable.DRILL_ID;
+import static com.armandgray.taap.db.DrillsTable.TABLE_DRILLS;
+
 class LogsTable {
 
     static final String TABLE_LOGS = "logs";
@@ -17,6 +20,19 @@ class LogsTable {
             LOG_ID, COLUMN_DATE, COLUMN_LENGTH, COLUMN_GOAL, COLUMN_ACTIVE_WORK,
             COLUMN_REST_TIME, COLUMN_SETS_COMPLETED, COLUMN_SUCCESS, COLUMN_DRILL };
 
-    static final String SQL_CREATE = "";
-
+    static final String SQL_CREATE =
+            "CREATE TABLE " + TABLE_LOGS + " ("
+                    + LOG_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + COLUMN_DATE + " INTEGER,"
+                    + COLUMN_LENGTH + " INTEGER, "
+                    + COLUMN_GOAL + " TEXT, "
+                    + COLUMN_ACTIVE_WORK + " INTEGER, "
+                    + COLUMN_REST_TIME + " INTEGER, "
+                    + COLUMN_SETS_COMPLETED + " INTEGER, "
+                    + COLUMN_REPS_COMPLETED + " INTEGER, "
+                    + COLUMN_SUCCESS + " REAL, "
+                    + COLUMN_DRILL + " INTEGER NOT NULL, "
+                    + "FOREIGN KEY (" + COLUMN_DRILL + ") REFERENCES "
+                    + TABLE_DRILLS + " (" + DRILL_ID + ")"
+                    + ")";
     }
