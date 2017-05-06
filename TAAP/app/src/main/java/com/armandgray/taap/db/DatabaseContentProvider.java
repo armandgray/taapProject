716c576cfce3d74af0901331eef6199e2a4aa0d2
Armand Gray <armandgray@gmail.com>
2017-05-06 10:51:48 -0700

    added passing extend clause

--- TAAP/app/src/main/java/com/armandgray/taap/db/DatabaseContentProvider.java
+++ TAAP/app/src/main/java/com/armandgray/taap/db/DatabaseContentProvider.java
@@ -1,10 +1,15 @@
 package com.armandgray.taap.db;
 
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.annotation.VisibleForTesting;
 
-public class DatabaseContentProvider {
+public class DatabaseContentProvider extends ContentProvider {
 
     public static final Uri CONTENT_URI_DRILLS;
     @VisibleForTesting static final String AUTHORITY = "com.armandgray.taap.db.provider";
@@ -13,4 +18,37 @@ public class DatabaseContentProvider {
     static { CONTENT_URI_DRILLS = Uri.parse("content://" + AUTHORITY + "/" + BASE_PATH_DRILLS); }
 
     @VisibleForTesting SQLiteDatabase database;
+
+    @Override
+    public boolean onCreate() {
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
 }
