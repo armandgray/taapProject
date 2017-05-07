33b026456122a9c02086179fc34769c82538b29c
Armand Gray <armandgray@gmail.com>
2017-05-07 09:43:33 -0700

    added test canReturnNullWhenPassedNull_GetArrayAsString

--- TAAP/app/src/test/java/com/armandgray/taap/utils/StringsHelperTest.java
+++ TAAP/app/src/test/java/com/armandgray/taap/utils/StringsHelperTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import static com.armandgray.taap.utils.StringsHelper.getArrayAsString;
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertNull;
 
 public class StringsHelperTest {
 
@@ -14,6 +15,11 @@ public class StringsHelperTest {
         String arrayAsString = getArrayAsString(array);
         assertNotNull(arrayAsString);
         assertEquals("a,ab,aab,abc", arrayAsString);
+    }
+
+    @Test
+    public void canReturnNullWhenPassedNull_GetArrayAsString() throws Exception {
+        assertNull(getArrayAsString(null));
     }
 
 }
