0a64bc85c4a37edde2be833b01486dc8fc585977
Armand Gray <armandgray@gmail.com>
2017-05-08 08:43:35 -0700

    Added test canGetAveragePercentage

--- TAAP/app/src/main/java/com/armandgray/taap/utils/MathHelper.java
+++ TAAP/app/src/main/java/com/armandgray/taap/utils/MathHelper.java
@@ -1,4 +1,10 @@
 package com.armandgray.taap.utils;
 
+import java.util.ArrayList;
+
 public class MathHelper {
+
+    public static double getAveragePercentage(ArrayList<Double> percentages) {
+        return 0.0;
+    }
 }
--- TAAP/app/src/test/java/com/armandgray/taap/utils/MathHelperTest.java
+++ TAAP/app/src/test/java/com/armandgray/taap/utils/MathHelperTest.java
@@ -1,5 +1,26 @@
 package com.armandgray.taap.utils;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static com.armandgray.taap.utils.MathHelper.getAveragePercentage;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+
 public class MathHelperTest {
+
+    @Test
+    public void canGetAveragePercentage() throws Exception {
+        ArrayList<Double> percentages = new ArrayList<>();
+        percentages.add(0.20);
+        percentages.add(0.40);
+        percentages.add(0.90);
+        double total = 0.0;
+        for (Double percent : percentages ) { total += percent; }
+
+        assertNotNull(getAveragePercentage(percentages));
+        assertEquals(total/percentages.size() , getAveragePercentage(percentages));
+    }
 
 }
