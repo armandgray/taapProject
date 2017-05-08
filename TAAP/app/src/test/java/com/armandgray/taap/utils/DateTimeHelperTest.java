6ddded69f9984228f8ef32ea201e5738a2b76455
Armand Gray <armandgray@gmail.com>
2017-05-08 08:22:54 -0700

    refactored dateTime methods from DetailActivityController to TimeTest

--- TAAP/app/src/test/java/com/armandgray/taap/utils/DateTimeHelperTest.java
+++ TAAP/app/src/test/java/com/armandgray/taap/utils/DateTimeHelperTest.java
@@ -1,5 +1,35 @@
 package com.armandgray.taap.utils;
 
+import org.junit.Test;
+
+import java.util.Calendar;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+
 public class DateTimeHelperTest {
+
+    @Test
+    public void canGetTimeElapsed() throws Exception {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(0, 0, 0, 0, 0, 0);
+        long dummyTime = System.currentTimeMillis();
+        calendar.setTimeInMillis(dummyTime);
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        if (hour > 12) { calendar.set(Calendar.HOUR_OF_DAY, 0); }
+        assertNotNull(controller.getTimeElapsedAsDate(dummyTime));
+        assertEquals(calendar.getTime(), controller.getTimeElapsedAsDate(dummyTime));
+    }
+
+    @Test
+    public void doesZeroOutHoursForTimesLessThanOneHour_CanGetTimeElapsed() throws Exception {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(0, 0, 0, 0, 0, 0);
+        calendar.setTimeInMillis(1);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+
+        assertNotNull(controller.getTimeElapsedAsDate(1));
+        assertEquals(calendar.getTime().toString(), controller.getTimeElapsedAsDate(1).toString());
+    }
 
 }
