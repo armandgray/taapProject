2109232de51a89063c1839b5b61394bda7ebc5ae
Armand Gray <armandgray@gmail.com>
2017-05-10 15:55:26 -0700

    added code to onCreateDialog to inflate layout

--- TAAP/app/src/main/java/com/armandgray/taap/detail/TimerDialog.java
+++ TAAP/app/src/main/java/com/armandgray/taap/detail/TimerDialog.java
@@ -1,11 +1,25 @@
 package com.armandgray.taap.detail;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.armandgray.taap.R;
 
 public class TimerDialog extends DialogFragment {
+
+    @NonNull
+    @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        return null;
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        @SuppressLint("InflateParams") View dialogLayout = LayoutInflater.from(getActivity())
+                .inflate(R.layout.timer_dialog_layout, null);
+        builder.setView(dialogLayout);
+        return builder.create();
     }
 }
