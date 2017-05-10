62f7e9555e294361e371c6a1a7b2f46b1e952927
Armand Gray <armandgray@gmail.com>
2017-05-10 13:26:00 -0700

    Added failing test canClickPositiveButtonToClearData_TestOnCreateDialog

--- TAAP/app/src/test/java/com/armandgray/taap/settings/ConfirmClearDataDialogTest.java
+++ TAAP/app/src/test/java/com/armandgray/taap/settings/ConfirmClearDataDialogTest.java
@@ -1,8 +1,14 @@
 package com.armandgray.taap.settings;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.armandgray.taap.BuildConfig;
+import com.armandgray.taap.db.DrillsTable;
+import com.armandgray.taap.db.LogsTable;
 
 import org.junit.After;
 import org.junit.Before;
@@ -14,6 +20,12 @@ import org.robolectric.RuntimeEnvironment;
 import org.robolectric.android.controller.ActivityController;
 import org.robolectric.annotation.Config;
 
+import static com.armandgray.taap.db.DatabaseContentProvider.CONTENT_URI_DRILLS;
+import static com.armandgray.taap.db.DatabaseContentProvider.CONTENT_URI_LOGS;
+import static com.armandgray.taap.db.DatabaseContentProvider.insertDrillToDatabase;
+import static com.armandgray.taap.db.DatabaseContentProvider.insertLogToDatabase;
+import static com.armandgray.taap.db.DatabaseContentProviderTest.TEST_SESSION_LOG;
+import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotNull;
 
 @RunWith(RobolectricTestRunner.class)
@@ -41,6 +53,38 @@ public class ConfirmClearDataDialogTest {
     public void canCreateConfirmClearDataDialog_TestOnCreateDialog() {
         Bundle savedInstanceState = new Bundle();
         assertNotNull(dialog.onCreateDialog(savedInstanceState));
+    }
+
+    @Test
+    public void canClickPositiveButtonToClearData_TestOnCreateDialog() {
+        insertDrillToDatabase(TEST_SESSION_LOG.getDrill(), RuntimeEnvironment.application);
+        insertDrillToDatabase(TEST_SESSION_LOG.getDrill(), RuntimeEnvironment.application);
+        insertDrillToDatabase(TEST_SESSION_LOG.getDrill(), RuntimeEnvironment.application);
+        insertLogToDatabase(TEST_SESSION_LOG, RuntimeEnvironment.application);
+        insertLogToDatabase(TEST_SESSION_LOG, RuntimeEnvironment.application);
+        insertLogToDatabase(TEST_SESSION_LOG, RuntimeEnvironment.application);
+
+        Bundle savedInstanceState = new Bundle();
+        AlertDialog resultDialog = (AlertDialog) dialog.onCreateDialog(savedInstanceState);
+        resultDialog.show();
+
+        Button btnPositive = resultDialog.getButton(DialogInterface.BUTTON_POSITIVE);
+        btnPositive.performClick();
+        resultDialog.dismiss();
+
+        Cursor drillCursor = RuntimeEnvironment.application.getContentResolver()
+                .query(CONTENT_URI_DRILLS, DrillsTable.ALL_DRILL_COLUMNS,
+                        null, null, null);
+        Cursor logCursor = RuntimeEnvironment.application.getContentResolver()
+                .query(CONTENT_URI_LOGS, LogsTable.ALL_LOG_COLUMNS,
+                        null, null, null);
+
+        assertNotNull(drillCursor);
+        assertNotNull(logCursor);
+        assertEquals(0, drillCursor.getCount());
+        assertEquals(0, logCursor.getCount());
+        drillCursor.close();
+        logCursor.close();
     }
 
     @After
