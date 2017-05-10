b2fc6946e4c85a2c9f007cd3fbe3fe5d3956337f
Armand Gray <armandgray@gmail.com>
2017-05-10 13:12:43 -0700

    added robo

--- TAAP/app/src/test/java/com/armandgray/taap/settings/ConfirmClearDataDialogTest.java
+++ TAAP/app/src/test/java/com/armandgray/taap/settings/ConfirmClearDataDialogTest.java
@@ -1,5 +1,13 @@
 package com.armandgray.taap.settings;
 
+import com.armandgray.taap.BuildConfig;
+
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class)
 public class ConfirmClearDataDialogTest {
 
 }
