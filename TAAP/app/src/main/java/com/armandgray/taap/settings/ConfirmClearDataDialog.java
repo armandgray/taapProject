5d4aaa4d09a39c361bc25a2e29be6303c46bed39
Armand Gray <armandgray@gmail.com>
2017-05-10 13:20:18 -0700

    added temporary constructor for context

--- TAAP/app/src/main/java/com/armandgray/taap/settings/ConfirmClearDataDialog.java
+++ TAAP/app/src/main/java/com/armandgray/taap/settings/ConfirmClearDataDialog.java
@@ -1,11 +1,20 @@
 package com.armandgray.taap.settings;
 
+import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
 
 public class ConfirmClearDataDialog {
 
+    private Context context;
+
+    public ConfirmClearDataDialog(Context context) {
+        this.context = context;
+    }
+
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
         return null;
     }
 
