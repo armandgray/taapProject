2df2602eb5ed2c221d3586f4f5bdd6f073e3013b
Armand Gray <armandgray@gmail.com>
2017-05-10 13:14:15 -0700

    added setup & teardown

--- TAAP/app/src/test/java/com/armandgray/taap/settings/ConfirmClearDataDialogTest.java
+++ TAAP/app/src/test/java/com/armandgray/taap/settings/ConfirmClearDataDialogTest.java
@@ -2,12 +2,36 @@ package com.armandgray.taap.settings;
 
 import com.armandgray.taap.BuildConfig;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.android.controller.ActivityController;
 import org.robolectric.annotation.Config;
 
 @RunWith(RobolectricTestRunner.class)
 @Config(constants = BuildConfig.class)
 public class ConfirmClearDataDialogTest {
+
+    private ActivityController<SettingsActivity> activityController;
+    private SettingsActivity activity;
+    private ConfirmClearDataDialog dialog;
+
+    @Before
+    public void setUp() {
+        System.out.println("Running Set Up!");
+        activityController = Robolectric.buildActivity(SettingsActivity.class);
+        activity = activityController.create().visible().get();
+        dialog = new ConfirmClearDataDialog();
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("Running TearDown!");
+        activityController.pause().stop().destroy();
+        activity = null;
+        dialog = null;
+    }
 
 }
