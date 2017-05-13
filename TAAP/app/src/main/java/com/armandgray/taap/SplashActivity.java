7441ccb3e779f8be405f26484d18cc944a5bb251
Armand Gray <armandgray@gmail.com>
2017-05-12 20:31:51 -0700

    added robo to SplashAct. test

--- TAAP/app/src/main/java/com/armandgray/taap/SplashActivity.java
+++ TAAP/app/src/main/java/com/armandgray/taap/SplashActivity.java
@@ -1,4 +1,6 @@
 package com.armandgray.taap;
 
-public class SplashActivity {
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
 }
--- TAAP/app/src/test/java/com/armandgray/taap/SplashActivityTest.java
+++ TAAP/app/src/test/java/com/armandgray/taap/SplashActivityTest.java
@@ -1,5 +1,32 @@
 package com.armandgray.taap;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.android.controller.ActivityController;
+import org.robolectric.annotation.Config;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class)
 public class SplashActivityTest {
+
+    private ActivityController<SplashActivity> activityController;
+    private SplashActivity activity;
+
+    @Before
+    public void setUp() {
+        System.out.println("Running Set Up!");
+        activityController = Robolectric.buildActivity(SplashActivity.class);
+        activity = activityController.create().visible().get();
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("Running TearDown!");
+        activityController.pause().stop().destroy();
+        activity = null;
+    }
 
 }
