582a091b00c668d0857f16576291a3ed3b782d45
Armand Gray <armandgray@gmail.com>
2017-05-23 12:54:34 -0700

    added initial code to test TestGet400OnNewDrillRoute

--- main_test.go
+++ main_test.go
@@ -2,8 +2,12 @@ package main_test
 
 import (
 	"testing"
+	"net/http"
 )
 
 func TestGet400OnNewDrillRoute(t *testing.T) {
-
+	_, err := http.NewRequest("GET", "/drills/new", nil)
+	if err != nil {
+    t.Fatal("Creating 'GET /questions/1/SC' request failed!")
+  }
 }
