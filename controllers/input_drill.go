06bac115b116a787fe87dd5b1028e1c2a566fd0c
Armand Gray <armandgray@gmail.com>
2017-05-24 20:05:41 -0700

    added template to InputController and rendered html

--- controllers/input_drill.go
+++ controllers/input_drill.go
@@ -1,9 +1,15 @@
 package controllers
 
 import (
+	"html/template"
 	"net/http"
 )
 
 func InputDrillController(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("input"))
+	templates := template.Must(template.ParseFiles("views/input_drill.html"))
+
+	if err := templates.ExecuteTemplate(w, "input_drill.html", nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
 }
