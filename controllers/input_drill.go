412db62245f09051cad407177670ddc19a669efa
Armand Gray <armandgray@gmail.com>
2017-05-24 20:11:29 -0700

    added page struct to controller

--- controllers/input_drill.go
+++ controllers/input_drill.go
@@ -5,10 +5,15 @@ import (
 	"net/http"
 )
 
+type Page struct {
+	Alert string
+}
+
 func InputDrillController(w http.ResponseWriter, r *http.Request) {
 	templates := template.Must(template.ParseFiles("views/input_drill.html"))
+	var page Page
 
-	if err := templates.ExecuteTemplate(w, "input_drill.html", nil); err != nil {
+	if err := templates.ExecuteTemplate(w, "input_drill.html", page); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 
