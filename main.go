942017ad9c49eaf82adde2c9fad3a88490c41aab
Armand Gray <armandgray@gmail.com>
2017-05-27 11:26:31 -0700

    added sql db reference to main.go and negroni middleware verify connection

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"net/http"
 	"taap_project/routes"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -9,13 +11,25 @@ import (
 	"github.com/urfave/negroni"
 )
 
+var db *sql.DB
+
 func main() {
+	db, _ = sql.Open("mysql", "root:#54nFr4nc15c0@/taap")
+
 	mux := gmux.NewRouter()
 	routes.AddDrillRoutes(mux)
 	routes.AddUserRoutes(mux)
 
 	n := negroni.Classic()
+	n.Use(negroni.HandlerFunc(VerifyMySQLConnection))
 	n.UseHandler(mux)
 	fmt.Println("Running...")
 	n.Run(":8181")
+}
+
+func VerifyMySQLConnection(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	if err := db.Ping(); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	next(w, r)
 }
