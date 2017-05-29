5d527ab69655953a8402ecebd845d64d7bd09b6b
Armand Gray <armandgray@gmail.com>
2017-05-28 18:09:37 -0700

    added tests for NewDrill

--- models/drill_test.go
+++ models/drill_test.go
@@ -1,1 +1,13 @@
 package models_test
+
+import (
+	. "taap_project/models"
+	"testing"
+)
+
+func TestNewDrill(t *testing.T) {
+	drill := Drill{"title", "category", 0}
+	if drill.Title != "title" && drill.Category != "category" && drill.ImageId != 0 {
+		t.Fail()
+	}
+}
