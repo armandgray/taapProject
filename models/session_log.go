c52c0841b9598be53c27872976f53e41591dfe74
Armand Gray <armandgray@gmail.com>
2017-06-03 15:40:05 -0700

    added gorp db tags to SessionLog model

--- models/session_log.go
+++ models/session_log.go
@@ -1,13 +1,13 @@
 package models
 
 type SessionLog struct {
-	LogId         int
-	Date          int
-	Goal          string
-	ActiveWork    int
-	RestTime      int
-	SetsCompleted int
-	RepsCompleted int
-	SuccessRate   float64
-	Drill         int
+	LogId         int     `db:"log_id"`
+	Date          int     `db:"date"`
+	Goal          string  `db:"goal"`
+	ActiveWork    int     `db:"active_work"`
+	RestTime      int     `db:"rest_time"`
+	SetsCompleted int     `db:"sets_completed"`
+	RepsCompleted int     `db:"reps_completed"`
+	SuccessRate   float64 `db:"success_rate"`
+	Drill         int     `db:"drill"`
 }
