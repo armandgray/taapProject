3a84cf142f4489fb317a0f8915aeb8a4369615ab
Armand Gray <armandgray@gmail.com>
2017-06-06 18:57:16 -0700

    added call to insert test drill into database in NewDrillController

--- controllers/new_drill.go
+++ controllers/new_drill.go
@@ -3,11 +3,15 @@ package controllers
 import (
 	"net/http"
 	"taap_project/helpers"
+	"taap_project/models"
 )
 
 func NewDrillController(w http.ResponseWriter, r *http.Request) {
 	if r.FormValue("submit") != "" {
-		w.Write([]byte("working"))
-		helpers.GetDatabase()
+		dbmap := helpers.GetGorpMap()
+		drill := models.Drill{"test", "test", 0}
+		if err := dbmap.Insert(&drill); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
 	}
 }
