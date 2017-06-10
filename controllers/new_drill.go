554797b3f84fefc4b3de45820f687fd0f6e22e2b
Armand Gray <armandgray@gmail.com>
2017-06-09 23:35:16 -0700

    added parsing code for int ImageId

--- controllers/new_drill.go
+++ controllers/new_drill.go
@@ -4,12 +4,19 @@ import (
 	"net/http"
 	"taap_project/helpers"
 	"taap_project/models"
+
+	"strconv"
 )
 
 func NewDrillController(w http.ResponseWriter, r *http.Request) {
 	if r.FormValue("submit") != "" {
+		var imageId int64
+		imageId, err := strconv.ParseInt(r.FormValue("imageId"), 10, 64)
+		if err != nil {
+			imageId = 0
+		}
 		dbmap := helpers.GetGorpMap()
-		drill := models.Drill{r.FormValue("title"), r.FormValue("category"), r.FormValue("imageId")}
+		drill := models.Drill{r.FormValue("title"), r.FormValue("category"), imageId}
 		if err := dbmap.Insert(&drill); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
