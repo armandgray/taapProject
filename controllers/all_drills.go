1996c7fd593455a6ea28b43a7e9b70207186eeb2
Armand Gray <armandgray@gmail.com>
2017-06-13 20:45:50 -0700

    added read code to AllDrillsController

--- controllers/all_drills.go
+++ controllers/all_drills.go
@@ -1,9 +1,26 @@
 package controllers
 
 import (
+	"taap_project/helpers"
+	"taap_project/models"
+
 	"net/http"
 )
 
 func AllDrillsController(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("All Drills"))
+	dbmap := helpers.GetGorpMap()
+	var drillList []models.Drill
+	if _, err := dbmap.Select(&drillList, "SELECT * FROM Drills"); err != nil {
+		w.Write(err.Error())
+		return
+	}
+
+	js, err := json.Marshal(userList)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
 }
